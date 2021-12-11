afddc78fa19aad2d15a0b00a6da334519546beda
donghwani <bkuekany@gmail.com>
2021-12-11 22:29:22 +0900

    feat: 사용자 입력값을 다루는 request dto 클래스

--- src/main/java/interfaces/dto/CalculatorRequest.java
+++ src/main/java/interfaces/dto/CalculatorRequest.java
@@ -2,19 +2,32 @@ package interfaces.dto;
 
 import interfaces.exception.BadRequestException;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class CalculatorRequest {
 
     private static final String WHITE_SPACE = " ";
     private static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
-    private final String[] input;
+    private List<Integer> numbers = new ArrayList<>();
+    private List<String> operators = new ArrayList<>();
 
     private CalculatorRequest(String[] input){
         if(isBlank(input) || validInputLength(input) || invalidPattern(input)) {
             throw new BadRequestException("잘못입력하셨습니다.");
         }
-        this.input = input;
+        setSeparateNumberAndOperator(input);
+    }
+
+    private void setSeparateNumberAndOperator(String[] input) {
+        for(int i = 0; i < input.length; i++) {
+            if(i % 2 == 0) {
+                numbers.add(Integer.parseInt(input[i]));
+            } else {
+                operators.add(input[i]);
+            }
+        }
     }
 
     private boolean invalidPattern(String[] input) {
@@ -40,5 +53,13 @@ public class CalculatorRequest {
 
     private boolean isBlank(String[] input) {
         return input == null || input.length == 0;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public List<String> getOperators() {
+        return operators;
     }
 }
